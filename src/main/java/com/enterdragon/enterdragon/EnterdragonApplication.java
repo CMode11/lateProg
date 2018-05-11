8c9716182cd250e392845494ddd98338bb49f7b6
Cmode11 <cathalmurray185@outlook.com>
2018-05-11 15:15:41 +0100

    koko  init commit

---
+++ src/main/java/com/enterdragon/enterdragon/EnterdragonApplication.java
@@ -0,0 +1,12 @@
+package com.enterdragon.enterdragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterdragonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterdragonApplication.class, args);
+    }
+}
